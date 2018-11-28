661c7e2409a5f1bc77e367d5f71bd6c98c6e30f1
kwanghyun.won <wonkwh@gmail.com>
2018-11-28 12:57:46 +0900

    add resource asset and ui interface design

---
+++ EggTimer/EggTimer/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  EggTimer
+//
+//  Created by wonkwh on 28/11/2018.
+//  Copyright © 2018 wonkwh. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
